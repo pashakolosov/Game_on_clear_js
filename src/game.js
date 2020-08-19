4e5ef1035ed846857b64a437ede74de7e9ed999e
pasha <pasha.colocov@gmail.com>
2020-08-19 16:10:57 +0300

    'finish'

--- src/game.js
+++ src/game.js
@@ -38,13 +38,14 @@ document.addEventListener('keydown', moveUp)
 function moveUp() {
     yPos -= 35
     fly.play()
-    console.log(xPos, yPos)
+    console.log(xPos, yPos, score)
 }
 
 //Bird position
 let xPos = 10
 let yPos = 150
 let grav = 1
+let score = 0
 
 function draw() {
     ctx.drawImage(bg, 0, 0)
@@ -68,9 +69,26 @@ function draw() {
                 y: Math.floor(Math.random() * pipeUp.height) - pipeUp.height
             })
         }
+
+        if (xPos + bird.width >= pipe[i].x
+            && xPos <= pipe[i].x + pipeUp.width 
+            && (yPos <= pipe[i].y + pipeUp.height 
+            || yPos + bird.height >= pipe[i].y + pipeUp.height + 
+            gap || yPos + bird.height >= cvs.height - fg.height)) {
+            location.reload()
+
+            console.log('hello')
+        }
+
+        if (pipe[i].x == 5) {
+            score++
+            score_audio.play()
+        }
     }
     
-
+    ctx.fillStyle = "#000"
+    ctx.font = "24px Verdana"
+    ctx.fillText("Score: " + score, 10, cvs.height - 20)
     
     requestAnimationFrame(draw)
 }
